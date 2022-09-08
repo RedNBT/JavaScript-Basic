5229b5bd4f4d958d664398fffcb080e006a44496
RedNBT <baotinnguyen4@gmail.com>
2022-09-08 17:10:31 +0700

    Source code chapter 2, video#9, bai5

---
+++ Video9/video9.js
@@ -0,0 +1,16 @@
+console.log("Hello world from HTML");
+
+//Dem chieu dai ky tu
+let text = "abcdasdsakhdbsakdb";
+console.log("Do Dai la:", text.length);
+
+//Tach chuoi
+let title = "Hello world from HTML";
+let split1 = title.split(" "); //Tach chuoi = khoang trong
+console.log(split1[2]);
+
+let split2 = title.split(""); //Tach chuoi = ky tu
+console.log(split2[6]);
+
+let split3 = title.split(); //Tach chuoi thanh Mang
+console.log(split3);
