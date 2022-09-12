6de78523e023401aa1dfc93bdca610b1902292ad
RedNBT <baotinnguyen4@gmail.com>
2022-09-12 21:03:20 +0700

    Source code chapter 2, video#10, bai6

---
+++ Video10/video10.js
@@ -0,0 +1,13 @@
+console.log('Hello world from HTML'.toLocaleUpperCase())
+let a = 5; //int, float, long, double
+let b = 6;
+let c = 6.9;
+let d = '5'
+console.log(a * d)
+console.log(a + d)
+console.log(a === d, 'a: ', typeof a, 'd: ', typeof d) //So sánh Chuỗi
+
+let e = Number(d); //Hoặc +(d) (Chuyển từ kiểu chuỗi sang số)
+console.log('e value: ', e, ' type: ', typeof e)
+
+console.log(a === e, 'a: ', typeof a, 'd: ', typeof d) //So sánh Chuỗi
