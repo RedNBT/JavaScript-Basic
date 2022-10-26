d9fec901bbd5e68d36caa1b48f3373ca69eb0d8f
RedNBT <baotinnguyen4@gmail.com>
2022-10-26 11:12:26 +0700

    Source code chapter 2, video#13, bai9 Array

---
+++ Video13/video13.js
@@ -0,0 +1,24 @@
+console.log('Hello world from HTML')
+let a = {
+    name: 'RedNBT',
+    age: 21
+}; //key:value
+
+console.log('check data object: ',a)
+console.log('my name\'s : ',a.name)
+console.log('my age is : ',a.age)
+
+//Object
+let c = {};
+
+
+//Array = [item1, item2, item3]
+let b = ['MU', 'Chelsea', "Liver"];
+console.log('typeof b: ', typeof b, b)
+console.log('first item: ', b[0], ' second item: ', b[1])
+
+let famaly = [
+    {name: 'Tin', age: '21'},
+    {name: 'Trung', age: '17'}
+];
+console.log('my famaly: ', famaly[0].name);
